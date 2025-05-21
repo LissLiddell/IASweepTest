bc80a82f9bb16f2ac5e53f430979268dee4d1f31
lilopez <lilopez@CATECINF214119D>
2025-05-21 15:50:25 -0600

    Crear prueba de suma de dos numeros

---
+++ index.js
@@ -0,0 +1,3 @@
+const sumar = (num1, num2) => num1 + num2
+
+console.log('sumar: ', sumar (5,8), ", esperado : 11");
---
+++ index.test.js
@@ -0,0 +1,11 @@
+describe('sumar', () => {
+    it('sumar dos numeros', () => {
+      const result = sumar(8, 5);
+      expect(result).toEqual(11);
+    });
+ 
+     it('sumar dos numeros', () => {
+      const result = sumar(-8, 4);
+      expect(result).toEqual(-4);
+    });
+  });
